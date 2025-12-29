5f028e8f03aa1b9fd2d7edcb7a2cc8d4c3a89e82
Ankush Kapoor <work.ankushkapoor1626@gmail.com>
2025-12-29 11:59:26 +0530

    added inference.ipynb
    also trained data

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ def get_config():
     # Original paper config given below
     return {
         "batch_size": 8,
-        "num_epochs": 20,
+        "num_epochs": 21,
         "lr": 10**-4,
         "seq_len": 350,
         "d_model": 512,
